f3c4bd2c0a76860baf98781e75f3638e3beb0c93
Gogol Bhattacharya <gogol.bhattacharya@kla-tencor.com>
2019-10-21 14:38:08 -0700

    Added in memory testing

--- src/main/scala/memory/Memory.scala
+++ src/main/scala/memory/Memory.scala
@@ -5,15 +5,16 @@ import chisel3._
 
 
 /** 
- * The Register File for the JOCEL processor implementation.
+ * The RAM for the JOCEL processor implementation.
  */
 class RAM (n:Int) extends Module {
   val io = IO(new Bundle {
     val WR_DATA         = Input(Vec(4, UInt(8.W)))
     val WR              = Input(Bool())
     val MASK            = Input(UInt(4))
-    val ADDR            = Input(UInt(sizeof(n-2).W))
-    val EN              = Input(Bool())
+    val WR_ADDR         = Input(UInt(sizeof(n-2).W))
+    val RD_ADDR         = Input(UInt(sizeof(n-2).W))
+    val RD              = Input(Bool())
 
     val RD_DATA         = Output(Vec(4, UInt(8.W)))
   })
@@ -21,9 +22,10 @@ class RAM (n:Int) extends Module {
   val mem = SyncReadMem(n/4, Vec(4, UInt(8.W)))
 
   when(io.WR) {
-    mem.write(io.ADDR, io.WR_DATA, io.MASK)
+    mem.write(io.WR_ADDR, io.WR_DATA, io.MASK)
     io.RD_DATA := DontCare
-  }.otherwise{
-    io.RD_DATA := mem.read(io.ADDR, io.EN)
+  }
+  when(io.RD){
+    io.RD_DATA := mem.read(io.RD_ADDR, io.RD)
   }
 }
--- src/test/scala/memory/MemoryUnitTester.scala
+++ src/test/scala/memory/MemoryUnitTester.scala
@@ -14,10 +14,43 @@ class MemoryTester(c: RAM) extends PeekPokeTester(c) {
   step(1)
 
   for(i <- 0 to 31) {
-    poke(ram.io.ADDR, i)
+    poke(ram.io.WR_ADDR, i)
     poke(ram.io.WR_DATA, i+10)
     poke(ram.io.MASK, 15); // Write to all columns
     step(1)
+  }
+
+
+  // Start Read Testing
+  poke(ram.io.WR, false);
+  poke(ram.io.RD, 1)
+  step(1)
+
+  for(i <- 0 to 31) {
+    poke(ram.io.RD_ADDR, i)
+    step(1)
+    expect(ram.io.RD_DATA, i+10)
+  }
+
+  // Now conduct syncronous testing.
+  // Load the first ram cell special.
+  poke(ram.io.WR, true);
+  poke(ram.io.WR_ADDR, 0)
+  poke(ram.io.WR_DATA, 11)
+  poke(ram.io.MASK, 15); // Write to all columns
+  step(1)
+
+  // For every cell check the previous row and make sure the address lines up with what we are expecting. 
+  for(i <- 1 to 31) {
+    // To the i'th row add the value i + 11.
+    poke(ram.io.WR_ADDR, i)
+    poke(ram.io.WR_DATA, i+11)
+    poke(ram.io.MASK, 15); // Write to all columns
+
+    // Check the previous row. It should have the value i+10
+    poke(ram.io.RD_ADDR, i-1)
+    step(1)
+    expect(ram.io.RD_DATA, i+10); 
   }
 
 }
