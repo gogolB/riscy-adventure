aaa25a2f2ccc0482d508361c031d67d246f71612
Gogol Bhattacharya <gogol.bhattacharya@kla-tencor.com>
2020-02-07 13:08:39 -0800

    Fixed address calculation

--- src/main/scala/memory/Memory.scala
+++ src/main/scala/memory/Memory.scala
@@ -6,15 +6,14 @@ import chisel3.util._
 
 
 /** 
- * The RAM for the JOCEL processor implementation.
+ * The RAM for the processor implementation.
  */
 class RAM (n:Int) extends Module {
   val io = IO(new Bundle {
     val WR_DATA         = Input(Vec(4, UInt(8.W)))
     val WR              = Input(Bool())
-    val MASK            = Input(Vec(4, Bool()))
-    val WR_ADDR         = Input(UInt(log2Ceil(n-2).W))
-    val RD_ADDR         = Input(UInt(log2Ceil(n-2).W))
+    val WR_ADDR         = Input(UInt(log2Ceil(n)-2.W))
+    val RD_ADDR         = Input(UInt(log2Ceil(n)-2.W))
     val RD              = Input(Bool())
 
     val RD_DATA         = Output(Vec(4, UInt(8.W)))
